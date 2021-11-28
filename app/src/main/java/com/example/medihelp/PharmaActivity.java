195b40bfbee4ad479cf1e2115d955448ba97b67a
SkyrunnerOO7 <punitpawar08@gmail.com>
2021-11-28 14:01:20 +0530

    doctor's appointment

--- app/src/main/java/com/example/medihelp/PharmaActivity.java
+++ app/src/main/java/com/example/medihelp/PharmaActivity.java
@@ -6,19 +6,47 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.net.Uri;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
 
 public class PharmaActivity extends AppCompatActivity {
     int Dose = 0;
     String name;
     String remark;
 
+    //for the adding the photo of the prescription
+    Button attachment;
+    TextView tvAttachment;
+    Uri URI = null;
+    private static final int PICK_FROM_GALLERY = 101;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pharma);
+
+        //for the adding the photo of the prescription
+        tvAttachment = findViewById(R.id.tvAttachment);
+        attachment = findViewById(R.id.btAttachment);
+        //attachment button listener
+        attachment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFolder();
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_FROM_GALLERY && resultCode == RESULT_OK) {
+            URI = data.getData();
+            tvAttachment.setText(URI.getLastPathSegment());
+            tvAttachment.setVisibility(View.VISIBLE);
+        }
     }
 
     public void submitOrder(View view) {
@@ -37,10 +65,24 @@ public class PharmaActivity extends AppCompatActivity {
         intent.putExtra(Intent.EXTRA_SUBJECT, "Medicine Summary ");
         intent.putExtra(Intent.EXTRA_TEXT,priceMessage);
 
+        //new image
+        //for the adding the photo of the prescription
+        if (URI != null) {
+            intent.putExtra(Intent.EXTRA_STREAM, URI);
+        }
+
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         }
 
+    }
+
+    public void openFolder() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.putExtra("return-data", true);
+        startActivityForResult(Intent.createChooser(intent, "Complete action using"), PICK_FROM_GALLERY);
     }
     /**
      * CreateMedicineSummary method
